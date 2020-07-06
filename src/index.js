24e62213f28186304883f1e58726c3b0ab6644da
rauzola <raul_sigoli@hotmail.com>
2020-07-06 14:06:37 -0300

    Diferenças do ReactJS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+
+// Não possuem valor semântico (significado)
+// Não possuem estilização própria
+// Todos componentes possuem por padrão "display: flex"
+
+// View: div, footer, header, main, asid, section
+// Text: p, span, strong, h1, h2, h3
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack</Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
